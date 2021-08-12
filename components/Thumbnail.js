943baa37191a10cc895a6ef309282c339d69e09b
Jubair <mjahmed.wd@gmail.com>
2021-08-12 15:00:23 +0600

    sss

--- components/Thumbnail.js
+++ components/Thumbnail.js
@@ -4,7 +4,7 @@ import React from "react";
 const Thumbnail = ({ result }) => {
   const BASE_URL = `https://image.tmdb.org/t/p/original`;
   return (
-    <div>
+    <div className="group m-5 p-5 cursor-pointer transition duration-200 ease-in-out transform sm:hover:scale-105 hover:z-1">
       <Image
         alt=""
         layout="responsive"
@@ -17,7 +17,7 @@ const Thumbnail = ({ result }) => {
       />
       <div className="p-2">
         <p className="truncate max-w-md">{result.overview}</p>
-        <h1>{result.title || result.original_name}</h1>
+        <h1 className="text-2xl mt-1 text-white transition-all duration-100 ease-in-out group-hover:font-bold">{result.title || result.original_name}</h1>
 
       </div>
     </div>
